94a79f83f5e685a4e4047abd81690c81306d0e12
James Taylor <jtx@visc.us>
2016-12-22 19:30:34 -0800

    images

--- htdocs/index.php
+++ htdocs/index.php
@@ -17,12 +17,29 @@ try {
 
     $teams = [];
 
-    foreach ($settings as $teamSettings) {
+    // Get the player name and their score
+    $teamAdditional = [];
+
+    foreach ($settings as $key => $teamSettings) {
         $teams[] = $parser->parseTeam($teamSettings);
+        $teamAdditional[$key]['name'] = $teamSettings->getName();
     }
 
+    foreach ($teams as $key => $players) {
+        $tmpScore = 0;
+        foreach ($players as $player) {
+            if (array_key_exists(4, $player)) {
+                $scoreMaybe = $player[4];
+                if (is_numeric($scoreMaybe)) {
+                    $tmpScore += $scoreMaybe;
+                }
+            }
+        }
+        $teamAdditional[$key]['score'] = $tmpScore;
+    }
 
-    echo $twig->render('index.twig', ['teams' => $teams]);
+    echo $twig->render('index.twig', ['teams' => $teams, 'teamAdditional' => $teamAdditional]);
+
 } catch(Exception $e) {
     sprintf("Well, the site appears dead: %s", $e->getMessage());
 }
