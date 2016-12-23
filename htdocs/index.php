2ec56ea8dfb81f90e991eab1540cce1b5d186101
James Taylor <jtx@Jamess-iMac.local>
2016-12-22 18:18:40 -0800

    twig stuff

--- htdocs/index.php
+++ htdocs/index.php
@@ -4,16 +4,20 @@ require_once('../vendor/autoload.php');
 require_once('../UnhappyHour/Parser.php');
 require_once('../UnhappyHour/GetSettings.php');
 
+$loader = new Twig_Loader_Filesystem('../templates');
+$twig = new Twig_Environment($loader);
+
 $iniFile = '../config/settings.ini';
 
 try {
     $settings = GetSettings::parseIni($iniFile);
-
     $parser = new Parser();
 
     foreach ($settings as $teamSettings) {
-        print_r($parser->parseTeam($teamSettings));
+//        print_r($parser->parseTeam($teamSettings));
     }
+
+    $twig->render('index.twig');
 } catch(Exception $e) {
     sprintf("Well, the site appears dead: %s", $e->getMessage());
 }
